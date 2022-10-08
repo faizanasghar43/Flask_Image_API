41e0e411a3d93d52b934075dbe6dc83a4904881b
Muhammad Faizan Asghar <81610362+faizanasghar43@users.noreply.github.com>
2022-10-08 20:10:41 +0500

    Update __init__.py

--- Flask_MicroService/service/__init__.py
+++ Flask_MicroService/service/__init__.py
@@ -9,22 +9,13 @@ app = Flask(__name__)
 
 
 def new_reader(encoded_img):
-<<<<<<< HEAD
     # decoding the base64 encoded image and opening it as an image
     img = Image.open(BytesIO(base64.b64decode(encoded_img)))
     qr_code = pyzbar.decode(img)[0]
     data = qr_code.data.decode("utf-8")
     type_ = qr_code.type
     return {"Type": type_, "Data": data}
-=======
-    img = Image.open(BytesIO(base64.b64decode(encoded_img)))    #decoding the base64 encoded image and opening it as an  image
-    qr_code = pyzbar.decode(img)[0]
-    data = qr_code.data.decode("utf-8")
-    type_ = qr_code.type
-    response = {"Type": type_, "Data": data}
-    return response
 
->>>>>>> 9860fe7aee2b2efc7bd2abcf6596ec8a2999a1a5
 
 
 from .routes import app
